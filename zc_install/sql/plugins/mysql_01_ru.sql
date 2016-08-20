e323177da3d279ad1a3da6522f8ea5c4b1679816
Andrew Berezin <andy.berezin@gmail.com>
2016-08-21 01:05:38 +0300

    Update mysql_01_ru.sql

--- zc_install/sql/plugins/mysql_01_ru.sql
+++ zc_install/sql/plugins/mysql_01_ru.sql
@@ -2,14 +2,14 @@
 # * SQL Localization script - Add ru language and set it as default languages
 # * @package Installer
 # * @access private
-# * @copyright Copyright 2004-2009 Andrew Berezin eCommerce-Service.com
-# * @copyright Copyright 2003-2009 Zen Cart Development Team
+# * @copyright Copyright 2004-2016 Andrew Berezin eCommerce-Service.com
+# * @copyright Copyright 2003-2016 Zen Cart Development Team
 # * @copyright Portions Copyright 2003 osCommerce
 # * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
 # * @Localization: Andrew Berezin http://ecommerce-service.com
-# * @version $Id: mysql_01_ru.sql 1.3.8a 17.12.2007 11:21 AndrewBerezin $
+# * @version $Id: mysql_01_ru.sql 1.5.5a 21.08.2016 1:01:07 AndrewBerezin $
 
 # Add Russian language
-REPLACE INTO languages VALUES (2,'Russian','ru','icon.gif','russian',1);
+INSERT INTO languages (languages_id, name, code, image, directory, sort_order) VALUES (NULL,'Russian','ru','icon.gif','russian',1);
 UPDATE languages SET sort_order=2 WHERE languages_id=1;
 UPDATE configuration SET configuration_value='ru' WHERE configuration_key='DEFAULT_LANGUAGE';
